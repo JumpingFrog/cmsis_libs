d18492ab1192e08cebcf3d91f901d9778c043bc5
David Wotherspoon <davidwother@hotmail.co.uk>
2013-10-31 17:39:53 +0000

    ADC.

--- adc_mbed.c
+++ adc_mbed.c
@@ -2,7 +2,7 @@
 
 PINSEL_CFG_Type adc_pcfg;
 
-void adc_init() {
+void adc_init(void) {
 	adc_pcfg.OpenDrain = 0;
 	adc_pcfg.Pinmode = 0;
 	adc_pcfg.Funcnum = 1;
@@ -13,23 +13,23 @@ void adc_enchan(uint8_t chan, uint8_t en) {
 	//first we set the pin (p15 to 20 on mbed)
 	switch(chan) {
 		case 0:
-			adc_pcfg.Pinnum = 0;
-			adc_pcfg.Portnum =	23;
+			adc_pcfg.Pinnum = 23;
+			adc_pcfg.Portnum =	0;
 			adc_pcfg.Funcnum = 1;
 			break;
 		case 1:
-			adc_pcfg.Pinnum = 0;
-			adc_pcfg.Portnum =	24;
+			adc_pcfg.Pinnum = 24;
+			adc_pcfg.Portnum = 0;
 			adc_pcfg.Funcnum = 1;
 			break;
 		case 2:
-			adc_pcfg.Pinnum = 0;
-			adc_pcfg.Portnum =	25;
+			adc_pcfg.Pinnum = 25;
+			adc_pcfg.Portnum =	0;
 			adc_pcfg.Funcnum = 1;
 			break;
 		case 3: //also DAC (func 2)(MUTEX).
-			adc_pcfg.Pinnum = 0;
-			adc_pcfg.Portnum =	26;
+			adc_pcfg.Pinnum = 26;
+			adc_pcfg.Portnum = 0;
 			adc_pcfg.Funcnum = 1;
 			break;
 		case 4: //actual audio socket on UoY host board
@@ -42,9 +42,13 @@ void adc_enchan(uint8_t chan, uint8_t en) {
 			adc_pcfg.Portnum =	1;
 			adc_pcfg.Funcnum = 2;
 			break;
+		default:
+			return;
 	}
-
-
-
+	PINSEL_ConfigPin(adc_pcfg);
 	ADC_ChannelCmd(chan, (en) ? Enable : Disable);
+}
+
+uint16_t adc_getdata(uint8_t chan) {
+	return ADC_ChannelGetData(ADC_DEV, chan);
 }
--- adc_mbed.h
+++ adc_mbed.h
@@ -4,3 +4,8 @@
 #define ADC_DEV LPC_ADC
 #define ADC_RATE 100000
 
+void adc_init(void);
+
+void adc_enchan(uint8_t chan, uint8_t en);
+
+uint16_t adc_getdata(uint8_t chan);
