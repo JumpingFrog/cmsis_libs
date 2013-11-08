21c9a01b3cf0019b397a17f54eca93bac1b1c640
JumpingFrog <davidwother@hotmail.co.uk>
2013-11-08 19:55:25 +0000

    Assertions.

--- dbg.c
+++ dbg.c
@@ -1,4 +1,5 @@
-void dbg_assert(char * msg, int test) {
-	if (!test) { //if assert fails
-	}
+void dbg_assert(char * msg, char * file, int line) {
+	tty_printf("Error assertion failed: %s:%d - %s\n\r", file, line, msg)
+	tty_dump_regs();
+	while (1) {} //lock up
 }
--- dbg.h
+++ dbg.h
@@ -1,4 +1,9 @@
-#define DBG_TTY LPC_UART0
+#include "tty_mbed.h"
 /* Debug library for mbed, provides assert etc */
 
-dbg_assert(char * msg, int test);
+//the main assert macro really important.
+#define ASSERT(X, Y) { \
+		if (X) {} \
+		else { dbg_assert(Y, __FILE__, __LINE__); } }
+
+void dbg_assert(char * msg, char * file, int line);
